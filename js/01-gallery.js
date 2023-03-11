d93b0b426ccd9f93dd48da87b0f395892b592253
ostfnk2401 <o.stfnk2401@gmail.com>
2023-03-11 18:18:40 +0200

    all tasks done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallerySection = document.querySelector(".gallery");
+
+gallerySection.addEventListener('click', onClick);
+
+let currentBasicLightbox = null;
+
+const makeGalleryContainerMarkup = galleryItems.map((image) => {
+    const { preview, original, description } = image;
+    return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+}).join("");
+
+gallerySection.insertAdjacentHTML("afterbegin", makeGalleryContainerMarkup);
+
+function onClick(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    event.preventDefault();
+    currentBasicLightbox = basicLightbox.create(`
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`)
+    currentBasicLightbox.show();
+    document.addEventListener('keydown', onEscPressed);
+}
+
+function onEscPressed(event) {
+    if (event.code === "Escape") {
+        currentBasicLightbox.close();
+        document.removeEventListener("keydown", onEscPressed);
+    }
+}
