afede90904be759de71683df1673758b78af9a76
ostfnk2401 <o.stfnk2401@gmail.com>
2023-03-15 14:18:15 +0200

    second task fixed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,6 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const gallerySection = document.querySelector(".gallery");
-
-gallerySection.addEventListener('click', onClick);
 
 const makeGalleryContainerMarkup = galleryItems.map((image) => {
     const { preview, original, description } = image;
@@ -21,28 +18,15 @@ const makeGalleryContainerMarkup = galleryItems.map((image) => {
 
 gallerySection.insertAdjacentHTML("afterbegin", makeGalleryContainerMarkup);
 
-function onClick(event) {
+const galleryLightBox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+galleryLightBox.on('show.simplelightbox', function (event) {
     if (event.target.nodeName !== 'IMG') {
         return;
     }
     event.preventDefault();
-    var lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
-}
-
-console.log(galleryItems);
-
-
-let gallery = new SimpleLightbox('.gallery a');
-gallery.on('show.simplelightbox', function () {
- // do something…
 });
 
-gallery.on('error.simplelightbox', function (e) {
- console.log(e); // some usefull information
-});
-
-// with jQuery nearly the same
-let gallery = $('.gallery a').simpleLightbox();
-gallery.on('show.simplelightbox', function () {
- // do something…
-});
